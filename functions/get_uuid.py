1a8970138589830a2e1787794dbecf5c0f06679a
nathphoenix <nathphoenix75@gmail.com>
2021-05-24 14:53:32 +0100

    updated

--- functions/get_uuid.py
+++ functions/get_uuid.py
@@ -9,11 +9,6 @@ def generate_uuid():
 
     today = datetime.now()
     today_date = datetime.strftime(today, '%Y-%m-%d %H:%M:%S:%f')
-
-    # Printing random id using uuid1()
-    print ("The random id using uuid1() is : ",end="")
-    #print (uuid.uuid4())
-
     generated_id = uuid.uuid4()
     generated_id = generated_id.hex
     data = {today_date:generated_id, "run_time": today_date}
