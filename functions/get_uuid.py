28dba72b06243ba811b627a0aa7de393c470f070
nathphoenix <nathphoenix75@gmail.com>
2021-05-24 12:57:10 +0100

    updated

--- functions/get_uuid.py
+++ functions/get_uuid.py
@@ -1,11 +1,11 @@
 import pymongo
 import uuid
 from datetime import datetime, date, time, timedelta
+client = pymongo.MongoClient()
+client = client['Bloverse_new_rss']
+uuid_collection = client['uuid_tables']
 
 def generate_uuid():
-    client = pymongo.MongoClient()
-    client = client['Bloverse_new_rss']
-    uuid_collection = client['uuid_tables']
 
     today = datetime.now()
     today_date = datetime.strftime(today, '%Y-%m-%d %H:%M:%S:%f')
@@ -15,7 +15,6 @@ def generate_uuid():
     #print (uuid.uuid4())
 
     generated_id = uuid.uuid4()
-    print(generated_id)
     generated_id = generated_id.hex
     data = {today_date:generated_id, "run_time": today_date}
     insert = uuid_collection.insert_one(data)
